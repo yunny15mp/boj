9c62945b0c5e8421065ea931bcf09e9135934a5e
yunny15mp <167666054+yunny15mp@users.noreply.github.com>
2024-04-21 13:44:45 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <array>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define X first
+#define Y second
+#define all(x) x.begin(), x.end()
+const ll MOD = 1e9 + 9;
+const ull INF = 1e18;
+
+ll dx[] = { -1, 1, -1, 1, 2, 2, -2, -2 };
+ll dy[] = { 2, 2, -2, -2, -1, 1, -1, 1 };
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    int tc = 1; //cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
